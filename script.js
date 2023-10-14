02c200845fb6386d1dc40ee80836485100ca2542
Sohan Pal <126942354+SohanP18@users.noreply.github.com>
2023-10-14 22:38:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -59,3 +59,53 @@ gsap.from(".aboutus img, .aboutusin",{
         scrub: 2
     }
 })
+
+
+gsap.from(".card",{
+    scale: 0.8,
+    opacity: 0,
+    duration: 1,
+    stagger:0.5,
+    scrollTrigger:{
+        trigger:".card",
+        scroller:"body",
+        start:"top 70%",
+        end:"top 65%",
+        scrub: 1
+    }
+})
+
+gsap.from("#colon1",{
+    y:-70,
+    x:-70,
+    scrollTrigger:{
+        trigger:"#colon1",
+        scroller:"body",
+        start:"top 55%",
+        end:"top 45%",
+        scrub: 4
+    }
+})
+
+gsap.from("#colon2",{
+    y:80,
+    x:80,
+    scrollTrigger:{
+        trigger:"#colon1",
+        scroller:"body",
+        start:"top 55%",
+        end:"top 45%",
+        scrub: 4
+    }
+})
+
+gsap.from("#page4 h1",{
+    y:15,
+    scrollTrigger:{
+        trigger:"#page4 h1",
+        scroller:"body",
+        start:"top 75%",
+        end:"top 70%",
+        scrub: 3
+    }
+})
